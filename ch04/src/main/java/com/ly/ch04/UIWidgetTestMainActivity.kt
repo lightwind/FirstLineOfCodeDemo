9ab4e32a682977d34bf115e9f580c19509d58e88
Allen <ly13395600086@163.com>
2023-02-05 20:59:44 +0800

    UI1

---
+++ ch04/src/main/java/com/ly/ch04/UIWidgetTestMainActivity.kt
@@ -0,0 +1,12 @@
+package com.ly.ch04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UIWidgetTestMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_ui_widget_test)
+    }
+}
